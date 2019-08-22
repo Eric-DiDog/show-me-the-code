ec1b5771293607b4faae98f1d3e49aef14a17259
Eric-DiDog <49754785+Eric-DiDog@users.noreply.github.com>
2019-08-22 15:29:40 +0800

    Add files via upload

--- 0011filtered_words.py
+++ 0011filtered_words.py
@@ -1,21 +1,22 @@
+#-*- coding: utf-8-*-
+
+
+
 import os
 import re
 
 
 
-def fileter(str):
-    for each in words:
-        if not str.find(each)==-1:
-            print('Freedom')
-            return True
-    print('Human Rights')
-    return False
+def fileter(input_words_list):
+    with open('filtered_words.txt', 'r', encoding='utf-8') as k:
+        words = k.read().splitlines()
+        k.close()
+    if not set(input_words_list).intersection(words):
+        print('Human Rights')
+    else:
+        print('Freedom')
 
-with open('filtered_words.txt', 'r', encoding='utf-8') as k:
-    words = k.read().splitlines()
-    k.close()
 
-word=''
-while not word.upper()=="EXIT":
-    word=input('输入敏感词:')
-    fileter(word)
+if __name__ == '__main__':
+    input_words_list = input('Input some words:').split()
+    fileter(input_words_list)
